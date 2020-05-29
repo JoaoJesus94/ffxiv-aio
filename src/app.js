b064644d8024a17f4a3660e8bbc82b9bece0a6e5
Joao Jesus <joaojesus_94@hotmail.com>
2020-05-29 19:28:56 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { AppRouter } from './routers/AppRouter'
+
+const App = () => <AppRouter />
+
+ReactDOM.render(<App />, document.getElementById('root'))
